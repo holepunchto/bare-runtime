68d0e0e14fc16c654fc384efbcbccb3547a713ff
Lauri Piisang <lauri.piisang@gmail.com>
2026-05-06 18:38:32 +0400

    fix: support pnpm runtime resolution

--- index.js
+++ index.js
@@ -14,21 +14,43 @@ module.exports = function runtime(referrer, opts) {
   const { platform = os.platform(), arch = os.arch() } = opts
 
   const filename = path.basename(referrer)
+  const pkg = `bare-runtime-${platform}-${arch}`
 
-  let mod
+  let pkgDir
   try {
-    mod = require(`bare-runtime-${platform}-${arch}`)
+    pkgDir = path.dirname(require.resolve(`${pkg}/package`))
   } catch (err) {
-    if (err.code === 'MODULE_NOT_FOUND') {
+    if (isModuleNotFound(err, `${pkg}/package`)) {
       throw new Error(`No binaries found for target '${platform}-${arch}'`)
     } else {
       throw err
     }
   }
 
+  const bin = resolvePackageBin(pkgDir, filename)
+
+  if (bin !== null) return bin
+
+  const mod = require(pkgDir)
+
   if (filename in mod === false) {
     throw new Error(`No binary found for target '${platform}-${arch}' for referrer '${referrer}'`)
   }
 
   return mod[filename]
+}
+
+function isModuleNotFound(err, request) {
+  return (
+    err &&
+    err.code === 'MODULE_NOT_FOUND' &&
+    err.message.startsWith(`Cannot find module '${request}'`)
+  )
+}
+
+function resolvePackageBin(pkgDir, filename) {
+  const pkg = require(path.join(pkgDir, 'package.json'))
+  const bin = pkg.bin && pkg.bin[filename]
+
+  return typeof bin === 'string' ? path.join(pkgDir, bin) : null
 }
